1f8b3d90a77383245d6528608f98b411bc41f51f
kauesilva07 <kauefsp@gmail.com>
2024-11-28 08:59:06 -0300

    Add files via upload

---
+++ flashcard-aula_10/kaue filipe/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'quem é o rei do futebol',
+    'é ele noss eterno rei Pelé'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital da Italia?',
+    'Roma'
+)
+
+criaCartao(
+    'anime',
+    'o que é jutsu?',
+    'é um conjunto de selos que fazem poder'
+)
+
+criaCartao(
+    'time',
+    'quando que foi o futebol foi criado',
+    '26 de outubro de 1863'
+)
